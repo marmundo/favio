26b77c09c6583c6423bfce4f2498ebe129f0b71e
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-09-22 08:11:12 -0300

    m

--- src/componentes/Conteudo2/index.js
+++ src/componentes/Conteudo2/index.js
@@ -1,22 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./Conteudo.module.css";
 
 const Conteudo = () => {
-  return (
-    <div>
-        Conteudo
-    <form onSubmit={()=>alert("Favorito salvo")}>
-        <label>
-            Nome do Site
-        </label>
-        <input name='nome_site'></input>
-        <label>
-            URL
-        </label>
-        <input type='url' name='url'></input>
-        <button >Salvar</button>
-    </form>
-    </div>
-  )
-}
+  const [nomeSite, setNomeSite] = useState("");
 
-export default Conteudo
+  return (
+    <div className={styles.area}>
+      Conteudo
+      <form
+        className={styles.formulario}
+        onSubmit={() => alert("Favorito salvo")}
+      >
+        <label>Nome do Site</label>
+        <input
+          name="nome_site"
+          onChange={(evento) => {
+            setNomeSite(evento.target.value);
+            console.log(nomeSite);
+          }}
+        ></input>
+        <br />
+        <label>URL</label>
+        <input type="url" name="url"></input>
+        <br />
+        <button>Salvar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Conteudo;
