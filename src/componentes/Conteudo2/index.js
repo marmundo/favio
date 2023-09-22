8fd72a43798bb51e07c05fb64dad20818cd75a6b
Marcelo Damasceno <marcelodamasceno@gmail.com>
2023-09-22 09:38:21 -0300

    adiciona estado

--- src/componentes/Conteudo2/index.js
+++ src/componentes/Conteudo2/index.js
@@ -3,6 +3,17 @@ import styles from "./Conteudo.module.css";
 
 const Conteudo = () => {
   const [nomeSite, setNomeSite] = useState("");
+  const [url, setUrl] = useState("");
+
+  let favoritos=[]
+
+  function salvarFavorito() {
+    console.log('favoritos>>>:',favoritos)
+    console.log('estados>>>:',{nomeSite,url})
+    favoritos=[...favoritos, {nomeSite,url}]
+    console.log('favoritos 2>>>:',favoritos)
+    localStorage.setItem("favorito", JSON.stringify(favoritos));
+  }
 
   return (
     <div className={styles.area}>
@@ -12,18 +23,24 @@ const Conteudo = () => {
         onSubmit={() => alert("Favorito salvo")}
       >
         <label>Nome do Site</label>
+        <h1>{nomeSite}</h1>
         <input
           name="nome_site"
           onChange={(evento) => {
             setNomeSite(evento.target.value);
-            console.log(nomeSite);
           }}
         ></input>
         <br />
         <label>URL</label>
-        <input type="url" name="url"></input>
+        <input
+          type="url"
+          name="url"
+          onChange={(evento) => {
+            setUrl(evento.target.value);
+          }}
+        ></input>
         <br />
-        <button>Salvar</button>
+        <button onClick={() => salvarFavorito()}>Salvar</button>
       </form>
     </div>
   );
